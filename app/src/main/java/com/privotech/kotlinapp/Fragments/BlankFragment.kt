84d1b576e7f15a20a1da1a4266899dc68a974f20
Usman Khan <usmannisarkhan@gmail.com>
2021-06-02 12:55:00 +0500

    Initial

--- app/src/main/java/com/privotech/kotlinapp/Fragments/BlankFragment.kt
+++ app/src/main/java/com/privotech/kotlinapp/Fragments/BlankFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.privotech.kotlinapp.databinding.FragmentBlankBinding
 import dagger.hilt.android.AndroidEntryPoint
 
