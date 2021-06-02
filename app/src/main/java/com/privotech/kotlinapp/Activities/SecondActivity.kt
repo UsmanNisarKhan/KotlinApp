8678f8e57eaa72ba2bcc1e573115256eeed282c6
Usman Khan <usmannisarkhan@gmail.com>
2021-06-02 15:10:54 +0500

    Initial

--- app/src/main/java/com/privotech/kotlinapp/Activities/SecondActivity.kt
+++ app/src/main/java/com/privotech/kotlinapp/Activities/SecondActivity.kt
@@ -29,12 +29,12 @@ import kotlin.random.Random
 @AndroidEntryPoint
 class SecondActivity : AppCompatActivity() , View.OnClickListener {
 
-    lateinit var binding : ActivitySecondBinding
+    lateinit var binding: ActivitySecondBinding
 
     lateinit var carAdapter: CarAdapter
 
     @Inject
-    lateinit var preferences : Preferences
+    lateinit var preferences: Preferences
 
 //    lateinit var arrayList : ArrayList<Car>                                                         // Array list not initialized
 
@@ -52,16 +52,17 @@ class SecondActivity : AppCompatActivity() , View.OnClickListener {
         binding = ActivitySecondBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val intent : Intent = getIntent()                                                             // Getting the value from intent
-        val value : Int = intent.getIntExtra("Val",0)
-        Toast.makeText(this,value.toString(),Toast.LENGTH_SHORT).show()
+        val intent: Intent = getIntent()                                                             // Getting the value from intent
+        val value: Int = intent.getIntExtra("Val",0)
+        Toast.makeText(this, value.toString(), Toast.LENGTH_SHORT).show()
 
 
-        var db = ApplicationDatabase.getDatabase(this@SecondActivity)
+        val db = ApplicationDatabase.getDatabase(this@SecondActivity)
         db?.carDao()?.getAll()?.observe(this, Observer {
 
-            binding.recyclerView.layoutManager = LinearLayoutManager(this,RecyclerView.VERTICAL,false)
-            carAdapter = CarAdapter(this,it)
+            binding.recyclerView.layoutManager =
+                LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+            carAdapter = CarAdapter(this, it)
             binding.recyclerView.adapter = carAdapter
 
         })
@@ -69,52 +70,55 @@ class SecondActivity : AppCompatActivity() , View.OnClickListener {
 
         binding.textView.text = "Welcome" + " " + Random.nextInt(5)                             // Generating Random number
 
-        var btn1  =  findViewById(R.id.btn1) as Button                                               // Initializing IDs
+        val btn1 = findViewById<Button>(R.id.btn1)                                                   // Initializing IDs
         btn1.setOnClickListener(this)
 
-        var btn2 : Button =  findViewById(R.id.btn2)
+        val btn2: Button = findViewById(R.id.btn2)
         btn2.setOnClickListener(this)
 
     }
 
     override fun onClick(v: View?) {
 
-        when(v?.id)
-        {
-            R.id.btn1 ->
-            {
+        when (v?.id) {
+            R.id.btn1 -> {
 //                arrayList = ArrayList<Car>()
-                arrayList.add(Car("4","4","1300"))                                            // Adding elements in Model Class
+                arrayList.add(Car("4", "4", "1300"))                                // Adding elements in Model Class
 
                 CoroutineScope(Dispatchers.IO).launch {
 
-                    var carClass = CarClass()
+                    val carClass = CarClass()
                     carClass.tyres = "4"
                     carClass.doors = "4"
                     carClass.power = "1300"
 
-                    var db = ApplicationDatabase.getDatabase(this@SecondActivity)
+                    val db = ApplicationDatabase.getDatabase(this@SecondActivity)
                     db?.carDao()?.insert(carClass)
                 }
 
 
-                Toast.makeText(this@SecondActivity,"Button 1 Clicked",Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@SecondActivity, "Button 1 Clicked", Toast.LENGTH_SHORT).show()
             }
-            R.id.btn2 ->
-            {
-                Toast.makeText(this@SecondActivity,"Button 2 Clicked",Toast.LENGTH_SHORT).show()
+            R.id.btn2 -> {
+                Toast.makeText(this@SecondActivity, "Button 2 Clicked", Toast.LENGTH_SHORT).show()
             }
 
         }
 
     }
 
-    companion object{                                                                                              // To declare Functions or Variables Static
+    companion object {                                                                               // To declare Functions or Variables Static
 
-        fun showValue(context : Context)
-        {
-            Toast.makeText(context,"Hi there i am static",Toast.LENGTH_SHORT).show()
+        fun showValue(context: Context) {
+            Toast.makeText(context, "Hi there i am static", Toast.LENGTH_SHORT).show()
         }
+
+    }
+
+    override fun onBackPressed() {
+
+        startActivity(Intent(this,MainActivity::class.java))
+        finish()
 
     }
 
